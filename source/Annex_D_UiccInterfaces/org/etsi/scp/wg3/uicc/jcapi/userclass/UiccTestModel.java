09c7a6cde4abfb8b2809e506a7dfcb3a663b798f
sebhans <sebastian.hans@oracle.com>
2022-03-31 19:11:28 +0200

    CR 48 SET(22)000059/SETTEST(22)071025r2 -Correction of UiccTestModel and UiccAPITestCardService

--- source/Annex_D_UiccInterfaces/org/etsi/scp/wg3/uicc/jcapi/userclass/UiccTestModel.java
+++ source/Annex_D_UiccInterfaces/org/etsi/scp/wg3/uicc/jcapi/userclass/UiccTestModel.java
@@ -9,4 +9,36 @@ package org.etsi.scp.wg3.uicc.jcapi.userclass;
 public abstract class UiccTestModel  {
 
     public abstract boolean run();
+
+    /**
+     * Initialises the results, removing all previous results.
+     */
+    protected final void initialiseResults()
+    {
+        UiccAPITestCardService.getTheUiccTestCardService().initialiseResults();
+    }
+
+    /**
+     * Adds a new result.
+     */
+    protected final void addResult(boolean result)
+    {
+        UiccAPITestCardService.getTheUiccTestCardService().addResult(result);
+    }
+
+    /**
+     * Returns the overall result, based on all results submitted via {@link #addResult(boolean)}
+     * since the last call to {@link #initialiseResults()}.
+     * <p>
+     * The following logic is used:
+     * <ul>
+     * <li> if no results have been submitted, <code>false</code> will be returned; </li>
+     * <li> if any <code>false</code> results have been submitted, <code>false</code> will be returned; </li>
+     * <li> otherwise, <code>true</code> will be returned. </li>
+     * </ul>
+     */
+    protected final boolean getOverallResult()
+    {
+        return UiccAPITestCardService.getTheUiccTestCardService().getOverallResult();
+    }
 }
