cec495842df96109a9a1cc3f11fb5cd443f1e013
sebhans <sebastian.hans@oracle.com>
2022-11-26 15:53:40 +0100

    CR 65 SET(22)000170 comment out ID3

--- source/Annex_E_SourceCode/uicc/test/catre/cre_apt_epha/Test_Cre_Apt_Epha.java
+++ source/Annex_E_SourceCode/uicc/test/catre/cre_apt_epha/Test_Cre_Apt_Epha.java
@@ -102,72 +102,72 @@ public class Test_Cre_Apt_Epha extends UiccTestModel {
         test.terminalProfileSession(UiccCardManagementService.DEFAULT_TERMINAL_PROFILE);
         test.deleteApplet(APPLET_AID_1);
         test.deleteApplet(APPLET_AID_2);
-		
-	    /*********************************************************************/
-        /** Testcase 3                                                       */
-        /*********************************************************************/
 
-        // Install Applet1   
-        response =  test.installApplet(CAP_FILE_PATH, CLASS_AID_1, APPLET_AID_1, 
-                           "8008" + // TLV UICC Toolkit application specific parameters
-                               "01" +   // V Priority Level
-                               "00" +   // V Max. number of timers
-                               "0A" +   // V Maximum text length for a menu entry
-                               "00" +   // V Maximum number of menu entries
-                               "00" +   // V Maximum number of channels 
-                               "00" +   // LV Minimum Security Level field
-                               "00" +   // LV TAR Value(s) 
-                               "00");   // V Maximum number of services
-							   
- 	    test.addResult(response.checkSw("9000")); 
-		
-        // Install Applet2    
-        response =  test.installApplet(CAP_FILE_PATH, CLASS_AID_2, APPLET_AID_2,    
-                           "8008" + // TLV UICC Toolkit application specific parameters
-                               "01" +   // V Priority Level
-                               "00" +   // V Max. number of timers
-                               "0A" +   // V Maximum text length for a menu entry
-                               "00" +   // V Maximum number of menu entries
-                               "00" +   // V Maximum number of channels 
-                               "00" +   // LV Minimum Security Level field
-                               "00" +   // LV TAR Value(s) 
-                               "00");   // V Maximum number of services
- 
-	    addResult(response.checkSw("9000"));  
-		
-        // Card Initialisation
-        test.reset();
-        test.terminalProfileSession("09030020 21");
-        
-        // Trigger the applets
-        response = test.envelopeEventProactiveHandlerAvailable();
-        addResult(response.checkSw("9000"));
-        
+// commented out Testcase 3, because envelopeEventProactiveHandlerAvailable() is currently FFS
+//	    /*********************************************************************/
+//        /** Testcase 3                                                       */
+//        /*********************************************************************/
+//
+//        // Install Applet1
+//        response =  test.installApplet(CAP_FILE_PATH, CLASS_AID_1, APPLET_AID_1,
+//                           "8008" + // TLV UICC Toolkit application specific parameters
+//                               "01" +   // V Priority Level
+//                               "00" +   // V Max. number of timers
+//                               "0A" +   // V Maximum text length for a menu entry
+//                               "00" +   // V Maximum number of menu entries
+//                               "00" +   // V Maximum number of channels
+//                               "00" +   // LV Minimum Security Level field
+//                               "00" +   // LV TAR Value(s)
+//                               "00");   // V Maximum number of services
+//
+// 	    test.addResult(response.checkSw("9000"));
+//
+//        // Install Applet2
+//        response =  test.installApplet(CAP_FILE_PATH, CLASS_AID_2, APPLET_AID_2,
+//                           "8008" + // TLV UICC Toolkit application specific parameters
+//                               "01" +   // V Priority Level
+//                               "00" +   // V Max. number of timers
+//                               "0A" +   // V Maximum text length for a menu entry
+//                               "00" +   // V Maximum number of menu entries
+//                               "00" +   // V Maximum number of channels
+//                               "00" +   // LV Minimum Security Level field
+//                               "00" +   // LV TAR Value(s)
+//                               "00");   // V Maximum number of services
+//
+//	    addResult(response.checkSw("9000"));
+//
+//        // Card Initialisation
+//        test.reset();
+//        test.terminalProfileSession("09030020 21");
+//
+//        // Trigger the applets
+//        response = test.envelopeEventProactiveHandlerAvailable();
+//        addResult(response.checkSw("9000"));
+//
+//
+//        /*********************************************************************/
+//        /*********************************************************************/
+//        /** Check Applets                                                    */
+//        /*********************************************************************/
+//        /*********************************************************************/
+//
+//        response = test.selectApplication(APPLET_AID_1);
+//        addResult(response.checkData("10" + APPLET_AID_1 + "01" + "CC"));
+//
+//
+//        /*********************************************************************/
+//        /*********************************************************************/
+//        /** Restore  card                                                    */
+//        /*********************************************************************/
+//        /*********************************************************************/
+//
+//        test.reset();
+//        test.terminalProfileSession(UiccCardManagementService.DEFAULT_TERMINAL_PROFILE);
+//        // delete applets and package
+//        test.deleteApplet(APPLET_AID_1);
+//        test.deleteApplet(APPLET_AID_2);
+        test.deletePackage(CAP_FILE_PATH);
 
-        /*********************************************************************/
-        /*********************************************************************/
-        /** Check Applets                                                    */
-        /*********************************************************************/
-        /*********************************************************************/
-
-        response = test.selectApplication(APPLET_AID_1);
-        addResult(response.checkData("10" + APPLET_AID_1 + "01" + "CC"));
-
-
-        /*********************************************************************/
-        /*********************************************************************/
-        /** Restore  card                                                    */
-        /*********************************************************************/
-        /*********************************************************************/
-
-        test.reset();
-        test.terminalProfileSession(UiccCardManagementService.DEFAULT_TERMINAL_PROFILE);
-        // delete applets and package
-        test.deleteApplet(APPLET_AID_1);
-        test.deleteApplet(APPLET_AID_2);
-        response = test.deletePackage(CAP_FILE_PATH);        
- 	    addResult(response.checkSw("9000"));
-        
 
         return getOverallResult();
     }
